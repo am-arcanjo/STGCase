a164f950626eb8ddb616fff79dd0fdbf0b6fe329
Arcanjo <amanda.arcanjo@parceiro.mrv.com.br>
2023-10-15 15:33:17 -0300

    ajustes lobby

--- Frontend/stage-case/src/pages/Lobby/index.js
+++ Frontend/stage-case/src/pages/Lobby/index.js
@@ -4,40 +4,17 @@ import {Link, useNavigate} from 'react-router-dom';
 import './style.css';
 
 function Lobby() {
-  const [searchText, setSearchText] = useState('');
-  const [searchResults, setSearchResults] = useState([]); 
-
-  const handleSearch = (e) => {
-    const text = e.target.value;
-    setSearchText(text);
-
-    const predefinedResults = ['Result 1', 'Result 2', 'Result 3']; 
-    const filteredResults = predefinedResults.filter(result =>
-      result.toLowerCase().includes(text.toLowerCase())
-    );
-
-    setSearchResults(filteredResults);
-  };
-
   return (
-    <div className="lobby">
-      <div className="search-bar">
-        <input
-          type="text"
-          placeholder="Procure a Área desejada"
-          value={searchText}
-          onChange={handleSearch}
-        />
-        <i className="fa fa-search"></i>
-      </div>
-      {searchResults.length > 0 && (
-        <ul className="search-results">
-          {searchResults.map((result, index) => (
-            <li key={index}>{result}</li>
-          ))}
-        </ul>
-      )}
+  <>
+    <div className="Background-layer">
+    <div>
+      <header className="Header">
+        Organizer/
+      </header>
     </div>
+      Áreas Cadastradas
+    </div>
+  </>
   );
 }
 
