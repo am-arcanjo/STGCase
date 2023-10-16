7869b54f7a7e66e582176916da97bea671cf0271
Arcanjo <amanda.arcanjo@parceiro.mrv.com.br>
2023-10-16 15:49:34 -0300

    ajustes lobby

--- Frontend/stage-case/src/pages/Lobby/index.js
+++ Frontend/stage-case/src/pages/Lobby/index.js
@@ -1,24 +1,54 @@
 import React, { useState } from 'react';
 import {Link, useNavigate} from 'react-router-dom';
+import { FiTrash2 } from 'react-icons/fi';
 
 import './style.css';
 const mockData = [
   {
-    areaName: 'lalala',
-    areaDescription: 'algo'
+    areaName: 'Recursos Humanos',
+    areaDescription: 'Área responsável por gerenciamento humano'
   },
   {
-    areaName: 'lalala2',
-    areaDescription: 'algo2'
+    areaName: 'Infraestrutura',
+    areaDescription: 'Responsável pela infraestrutura das operações'
   },
   {
-    areaName: 'lalala3',
-    areaDescription: 'algo3'
+    areaName: 'Limpeza',
+    areaDescription: 'Consiste das pessoas envolvidas na limpeza e higienização do escritório como um todo'
   }
 ];
 
-
 function Lobby() {
+
+  // async function handleDeleteArea(id) {
+//   try {
+//       await api.delete(`area/${id}`, {
+//       });
+
+//       setArea(area.filter(area => area.id !== id));
+
+//   } catch (err) {
+//       alert('Erro ao deletar Área, tente novamente.');
+//   }
+// }
+
+// const [areas, setAreas] = useState([]);
+
+// const navigate = useNavigate();
+
+// const areaId = localStorage.getItem('areaId');
+// const areaName = localStorage.getItem('areaName');
+
+// useEffect(() => {
+//     api.get('profile', {
+//         headers: {
+//             Authorization: areaId,
+//         }
+//     }).then(response => {
+//         setAreas(response.data);
+//     })
+// }, [areaId]);
+
   return (
     <div className="Background-layer">
     <div>
@@ -30,18 +60,21 @@ function Lobby() {
       </header>
     </div>
       Áreas Cadastradas
-      <div>
+      <ul className="Card">
         {mockData.map(area => {
-          return <div className="Card">
-            <span>
+          return <li>
+            <p>
               {area.areaName}
-            </span>
-            <span>
+            </p>
+            <p>
               {area.areaDescription}
-            </span>
-          </div>
+            </p>
+            <button /*onClick={() => handleDeleteArea(area.id)}*/ type="button">
+                        <FiTrash2 size={25} color="#a8a8b3"/>
+            </button>
+          </li>
         })}
-      </div>
+      </ul>
     </div>
   );
 }
