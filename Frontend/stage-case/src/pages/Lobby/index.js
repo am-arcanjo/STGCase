d4652520b94e2afbbdf218aed28061b4656ff924
Arcanjo <amanda.arcanjo@parceiro.mrv.com.br>
2023-10-16 16:37:06 -0300

    ajustes lobby cards

--- Frontend/stage-case/src/pages/Lobby/index.js
+++ Frontend/stage-case/src/pages/Lobby/index.js
@@ -1,80 +1,97 @@
-import React, { useState } from 'react';
-import {Link, useNavigate} from 'react-router-dom';
-import { FiTrash2 } from 'react-icons/fi';
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { FiTrash2, FiEdit } from "react-icons/fi";
 
-import './style.css';
+import "./style.css";
 const mockData = [
   {
-    areaName: 'Recursos Humanos',
-    areaDescription: 'Área responsável por gerenciamento humano'
+    areaName: "Recursos Humanos",
+    areaDescription: "Área responsável por gerenciamento humano",
   },
   {
-    areaName: 'Infraestrutura',
-    areaDescription: 'Responsável pela infraestrutura das operações'
+    areaName: "Infraestrutura",
+    areaDescription: "Responsável pela infraestrutura das operações",
   },
   {
-    areaName: 'Limpeza',
-    areaDescription: 'Consiste das pessoas envolvidas na limpeza e higienização do escritório como um todo'
-  }
+    areaName: "Limpeza",
+    areaDescription:
+      "Consiste das pessoas envolvidas na limpeza e higienização do escritório como um todo",
+  },
+  {
+    areaName: "Alguma outra área",
+    areaDescription:
+      "Consiste em ser alguma outra área pra finalidade de teste",
+  },
 ];
 
 function Lobby() {
-
   // async function handleDeleteArea(id) {
-//   try {
-//       await api.delete(`area/${id}`, {
-//       });
+  //   try {
+  //       await api.delete(`area/${id}`, {
+  //       });
 
-//       setArea(area.filter(area => area.id !== id));
+  //       setArea(area.filter(area => area.id !== id));
 
-//   } catch (err) {
-//       alert('Erro ao deletar Área, tente novamente.');
-//   }
-// }
+  //   } catch (err) {
+  //       alert('Erro ao deletar Área, tente novamente.');
+  //   }
+  // }
 
-// const [areas, setAreas] = useState([]);
+  // const [areas, setAreas] = useState([]);
 
-// const navigate = useNavigate();
+  // const navigate = useNavigate();
 
-// const areaId = localStorage.getItem('areaId');
-// const areaName = localStorage.getItem('areaName');
+  // const areaId = localStorage.getItem('areaId');
+  // const areaName = localStorage.getItem('areaName');
 
-// useEffect(() => {
-//     api.get('profile', {
-//         headers: {
-//             Authorization: areaId,
-//         }
-//     }).then(response => {
-//         setAreas(response.data);
-//     })
-// }, [areaId]);
+  // useEffect(() => {
+  //     api.get('profile', {
+  //         headers: {
+  //             Authorization: areaId,
+  //         }
+  //     }).then(response => {
+  //         setAreas(response.data);
+  //     })
+  // }, [areaId]);
 
   return (
     <div className="Background-layer">
-    <div>
-      <header className="Header">
-        Organizer/
-
-        <Link to="/" className="Register-button">Cadastrar Área</Link>
-
-      </header>
-    </div>
+      <div>
+        <header className="Header">
+          Organizer/
+          <Link to="/" className="Register-button">
+            Cadastrar Área
+          </Link>
+        </header>
+      </div>
       Áreas Cadastradas
       <ul className="Card">
-        {mockData.map(area => {
-          return <li>
-            <p>
-              {area.areaName}
-            </p>
-            <p>
-              {area.areaDescription}
-            </p>
-            <button /*onClick={() => handleDeleteArea(area.id)}*/ type="button">
-                        <FiTrash2 size={25} color="#a8a8b3"/>
-            </button>
-          </li>
+        {mockData.map((area) => {
+          return (
+            <li>
+              <div>
+                <h3>{area.areaName}</h3>
+                <div>
+                  <button
+                    className="Delete-button"
+                    /*onClick={() => handleDeleteArea(area.id)}*/ type="button"
+                  >
+                    <FiTrash2 size="25" />
+                  </button>
+                  <button
+                    className="Edit-button"
+                    /*onClick={() => handleUpdateArea(area.id)}*/ type="button"
+                  >
+                    <FiEdit size="25" />
+                  </button>
+                </div>
+              </div>
+              <p>{area.areaDescription}</p>
+            </li>
+          );
         })}
       </ul>
+      <a className="pagination"></a>
     </div>
   );
 }
