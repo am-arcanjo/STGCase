9182d464c2a40c9f1a3f488496362109f54aff5e
Arcanjo <amanda.arcanjo@parceiro.mrv.com.br>
2023-10-16 18:56:46 -0300

    mexendo lugares arquivos

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -1,15 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -23,3 +22,10 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+public void ConfigureServices(IServiceCollection services)
+{
+    services.AddDbContext<AreaDbContext>(options =>
+        options.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
+}
+
