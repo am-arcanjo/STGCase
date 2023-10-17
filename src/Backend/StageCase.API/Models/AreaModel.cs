b31c2842b67b28958ee0e9d2b62273bc1a4a72ac
Arcanjo <amanda.arcanjo@parceiro.mrv.com.br>
2023-10-17 09:19:09 -0300

    database migrations

--- src/Backend/StageCase.API/Models/AreaModel.cs
+++ src/Backend/StageCase.API/Models/AreaModel.cs
@@ -3,6 +3,7 @@ using StageCase.API;
 
 namespace StageCase.API {
     public class Area {
+        public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
     }
